5ae6207bc75d127e6a63eb131a9b411bce1967c6
Aleksey <alekseymishuk@gmail.com>
2018-10-02 00:45:59 +0300

    semi realization

--- src/index.js
+++ src/index.js
@@ -1,27 +1,81 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.value = initialValue;
+    this.operationQueue = [];
   }
 
-  add(number) {
-    // your implementation
+  add(number, secondValue = null) {
+    if ( secondValue === null) {
+      this.operationQueue.push({operation: this.add, value: number});
+      return this;
+    }
+    return number + secondValue;
   }
   
-  subtract(number) {
-    // your implementation
+  subtract(number, secondValue = null) {
+    if ( secondValue === null) {
+      this.operationQueue.push({operation: this.subtract, value: number});
+      return this;
+    }
+    return number - secondValue;
   }
+  
 
   multiply(number) {
-    // your implementation
+    const lastOperation = this.operationQueue.pop();
+    
+    if (!lastOperation) {
+      this.value *= number; 
+      return this;
+    }
+    
+    this.operationQueue.push({operation: lastOperation.operation, value: lastOperation.value * number});
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    const lastOperation = this.operationQueue.pop();
+    
+    if (!lastOperation) {
+      this.value /= number; 
+      return this;
+    }
+    
+    this.operationQueue.push({operation: lastOperation.operation, value: lastOperation.value / number});
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    const lastOperation = this.operationQueue.pop();
+    
+    if (!lastOperation) {
+      this.value = Math.pow(this.value, number); 
+      return this;
+    }
+    
+    this.operationQueue.push({operation: lastOperation.operation, value: Math.pow(lastOperation.value, number)});
+    return this;
   }
+
+  valueOf() {
+    //return this.value;
+    this.value = this.operationQueue.reduce((accumulator, item) => {
+      return item.operation(accumulator, item.value);
+    }, this.value);
+    return this.value;
+  }
+  
 }
 
-module.exports = SmartCalculator;
+//module.exports = SmartCalculator;
+
+let calc = new SmartCalculator(10);
+const x = calc.multiply(2)
+.pow(2)
+.subtract(95)
+.subtract(56)
+.pow(2)
+.pow(2)
+.pow(1)
+.multiply(1);
+console.log(x);
