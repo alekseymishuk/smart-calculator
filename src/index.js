63cd63e333291b142ccc8f0935dacb3a0d9e4660
alekseyMishuk <alekseymishuk@gmail.com>
2018-10-02 18:05:19 +0300

    implemented SmartCalculator

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ class SmartCalculator {
 
   add(number, secondValue = null) {
     if ( secondValue === null) {
-      this.operationQueue.push({operation: this.add, value: number});
+      this.operationQueue.push({operation: this.add, value: number, priority: 2});
       return this;
     }
     return number + secondValue;
@@ -14,68 +14,72 @@ class SmartCalculator {
   
   subtract(number, secondValue = null) {
     if ( secondValue === null) {
-      this.operationQueue.push({operation: this.subtract, value: number});
+      this.operationQueue.push({operation: this.subtract, value: number, priority: 2});
       return this;
     }
     return number - secondValue;
   }
   
 
-  multiply(number) {
-    const lastOperation = this.operationQueue.pop();
-    
-    if (!lastOperation) {
-      this.value *= number; 
+  multiply(number, secondValue = null) {
+    if (secondValue === null) {
+      this.operationQueue.push({operation: this.multiply, value: number, priority: 1});
       return this;
     }
-    
-    this.operationQueue.push({operation: lastOperation.operation, value: lastOperation.value * number});
-    return this;
+    return number * secondValue;
   }
 
-  devide(number) {
-    const lastOperation = this.operationQueue.pop();
-    
-    if (!lastOperation) {
-      this.value /= number; 
+  devide(number, secondValue = null) {
+    if (secondValue === null) {
+      this.operationQueue.push({operation: this.devide, value: number, priority: 1});
       return this;
     }
-    
-    this.operationQueue.push({operation: lastOperation.operation, value: lastOperation.value / number});
-    return this;
+    return number / secondValue;
   }
 
-  pow(number) {
-    const lastOperation = this.operationQueue.pop();
-    
-    if (!lastOperation) {
-      this.value = Math.pow(this.value, number); 
+  pow(number, secondValue = null) {
+    if (secondValue === null) {
+      this.operationQueue.push({operation: this.pow, value: number, priority: 0});
       return this;
     }
-    
-    this.operationQueue.push({operation: lastOperation.operation, value: Math.pow(lastOperation.value, number)});
-    return this;
+    return Math.pow(number, secondValue);
   }
 
-  valueOf() {
-    //return this.value;
+  calculateValue() {
+    for (let priorityIndex = 0; priorityIndex < 2; priorityIndex ++) {
+      
+      while(this.operationQueue.some(it => it.priority === priorityIndex)) {
+        let operationIndex = -1;
+        if (priorityIndex === 0 ) operationIndex = this.operationQueue.map(it => it.priority === priorityIndex).lastIndexOf(true);
+
+        const currentOperation = priorityIndex > 0 ? this.operationQueue.find((it, index) => {
+          if (it.priority === priorityIndex) {
+            operationIndex = index;
+            return true;
+          }
+          return false;
+        }) : this.operationQueue[operationIndex];
+
+        if (operationIndex === 0)
+          this.value = currentOperation.operation(this.value, currentOperation.value);
+        else
+          this.operationQueue[operationIndex - 1].value = 
+          currentOperation.operation(this.operationQueue[operationIndex - 1].value, currentOperation.value);
+
+        this.operationQueue = this.operationQueue.filter(it => it != currentOperation);
+      }
+    }
+
     this.value = this.operationQueue.reduce((accumulator, item) => {
       return item.operation(accumulator, item.value);
     }, this.value);
+  }
+
+  valueOf() {
+    this.calculateValue();
     return this.value;
   }
   
 }
 
-//module.exports = SmartCalculator;
-
-let calc = new SmartCalculator(10);
-const x = calc.multiply(2)
-.pow(2)
-.subtract(95)
-.subtract(56)
-.pow(2)
-.pow(2)
-.pow(1)
-.multiply(1);
-console.log(x);
+module.exports = SmartCalculator;
